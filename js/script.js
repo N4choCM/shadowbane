9a5e1a9d027c2cee5a400743feee854d6a72c0a6
N4choCM <nachocamposmarti@gmail.com>
2023-06-23 08:20:32 +0200

    ⬆ UPDATED: New logic 4 music & other minor issues

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 // This code creates a map for a game. The map is a 2D array of numbers that correspond to different tiles. The code uses the numbers to determine which color to use for each tile. The map is displayed on the screen using a canvas.
 let countdown = 30;
 let countdownInterval;
+let playerMoved = false;
+let countdownStarted = false;
+let gameStarted = false;
+let gameStartedForProtagonist = true;
 let mainInterval;
 let canvas;
 let ctx;
@@ -48,49 +52,47 @@ const drawMap = () => {
 	}
 };
 
-let light = function(x, y) {
-    this.x = x;
-    this.y = y;
-    this.delay = 50;
-    this.index = 0;
-    this.frame = 0;
-    this.changeFrame = () => {
-        if(this.frame < 3){
-            this.frame++;
-        }else{
-            this.frame = 0;
-        }
-    }
-    this.draw = () => {
-        if (this.index < this.delay) {
-            this.index++;
-        } else {
-            this.index = 0;
-            this.changeFrame();
-        }
-    
-        ctx.drawImage(
-            tileMap,
-            this.frame * 32,
-            64,
-            32,
-            32,
-            this.x * width,
-            this.y * height,
-            width,
-            height
-        );
-    };
-    
-}
+let light = function (x, y) {
+	this.x = x;
+	this.y = y;
+	this.delay = 50;
+	this.index = 0;
+	this.frame = 0;
+	this.changeFrame = () => {
+		if (this.frame < 3) {
+			this.frame++;
+		} else {
+			this.frame = 0;
+		}
+	};
+	this.draw = () => {
+		if (this.index < this.delay) {
+			this.index++;
+		} else {
+			this.index = 0;
+			this.changeFrame();
+		}
 
+		ctx.drawImage(
+			tileMap,
+			this.frame * 32,
+			64,
+			32,
+			32,
+			this.x * width,
+			this.y * height,
+			width,
+			height
+		);
+	};
+};
 
 let enemy = function (x, y) {
 	this.x = x;
 	this.y = y;
 	this.direction = Math.floor(Math.random() * 4);
-    this.delay = 50;
-    this.frame = 0;
+	this.delay = 50;
+	this.frame = 0;
 	this.draw = () => {
 		ctx.drawImage(
 			tileMap,
@@ -112,44 +114,44 @@ let enemy = function (x, y) {
 		return enemyCollision;
 	};
 	this.moveRandomly = () => {
-        protagonist.checkCollisionWithEnemy(this.x, this.y);
-        if(this.index < this.delay){
-            this.index++;
-        }else{
-            this.index = 0;
-            // ARRIBA
-            if (this.direction == 0) {
-                if (!this.checkEnemyCollision(this.x, this.y - 1)) {
-                    this.y--;
-                } else {
-                    this.direction = Math.floor(Math.random() * 4);
-                }
-            }
-            // ABAJO
-            if (this.direction == 1) {
-                if (!this.checkEnemyCollision(this.x, this.y + 1)) {
-                    this.y++;
-                } else {
-                    this.direction = Math.floor(Math.random() * 4);
-                }
-            }
-            // IZQUIERDA
-            if (this.direction == 2) {
-                if (!this.checkEnemyCollision(this.x - 1, this.y)) {
-                    this.x--;
-                } else {
-                    this.direction = Math.floor(Math.random() * 4);
-                }
-            }
-            // DERECHA
-            if (this.direction == 3) {
-                if (!this.checkEnemyCollision(this.x + 1, this.y)) {
-                    this.x++;
-                } else {
-                    this.direction = Math.floor(Math.random() * 4);
-                }
-            }
-        }
+		protagonist.checkCollisionWithEnemy(this.x, this.y);
+		if (this.index < this.delay) {
+			this.index++;
+		} else {
+			this.index = 0;
+			// ARRIBA
+			if (this.direction == 0) {
+				if (!this.checkEnemyCollision(this.x, this.y - 1)) {
+					this.y--;
+				} else {
+					this.direction = Math.floor(Math.random() * 4);
+				}
+			}
+			// ABAJO
+			if (this.direction == 1) {
+				if (!this.checkEnemyCollision(this.x, this.y + 1)) {
+					this.y++;
+				} else {
+					this.direction = Math.floor(Math.random() * 4);
+				}
+			}
+			// IZQUIERDA
+			if (this.direction == 2) {
+				if (!this.checkEnemyCollision(this.x - 1, this.y)) {
+					this.x--;
+				} else {
+					this.direction = Math.floor(Math.random() * 4);
+				}
+			}
+			// DERECHA
+			if (this.direction == 3) {
+				if (!this.checkEnemyCollision(this.x + 1, this.y)) {
+					this.x++;
+				} else {
+					this.direction = Math.floor(Math.random() * 4);
+				}
+			}
+		}
 	};
 };
 
@@ -186,6 +188,12 @@ let player = function () {
 		if (!this.controlMargins(this.x, this.y - 1)) {
 			this.y--;
 			this.checkKeyAndDoor();
+			playerMoved = true;
+			if (!countdownStarted) {
+				countdownStarted = true;
+				gameStarted = true;
+				updateCountdown();
+			}
 		}
 	};
 	// This code controls the player's movement to go down with the keyboard.
@@ -193,6 +201,12 @@ let player = function () {
 		if (!this.controlMargins(this.x, this.y + 1)) {
 			this.y++;
 			this.checkKeyAndDoor();
+			playerMoved = true;
+			if (!countdownStarted) {
+				countdownStarted = true;
+				gameStarted = true;
+				updateCountdown();
+			}
 		}
 	};
 	// This code controls the player's movement to go left with the keyboard.
@@ -200,6 +214,12 @@ let player = function () {
 		if (!this.controlMargins(this.x - 1, this.y)) {
 			this.x--;
 			this.checkKeyAndDoor();
+			playerMoved = true;
+			if (!countdownStarted) {
+				countdownStarted = true;
+				gameStarted = true;
+				updateCountdown();
+			}
 		}
 	};
 	// This code controls the player's movement to go right with the keyboard.
@@ -207,33 +227,47 @@ let player = function () {
 		if (!this.controlMargins(this.x + 1, this.y)) {
 			this.x++;
 			this.checkKeyAndDoor();
+			playerMoved = true;
+			if (!countdownStarted) {
+				countdownStarted = true;
+				gameStarted = true;
+				updateCountdown();
+			}
 		}
 	};
 	// This code controls the player's movement to win the game.
 	this.win = () => {
 		this.key = false;
+		playerMoved = false;
+		countdownStarted = false;
+		gameStarted = false;
+		gameStartedForProtagonist = false;
 		resetEnemies();
 		resetCountdown();
 		Swal.fire({
-			title: 'You won!',
-			text: 'Do you want to continue?',
-			icon: 'success',
-			confirmButtonText: 'Go to the next level!'
-		  }).then(() => {
+			title: "You won!",
+			text: "Do you want to continue?",
+			icon: "success",
+			confirmButtonText: "Go to the next level!",
+		}).then(() => {
 			restartLevel();
 		});
 		this.x = 1;
 		this.y = 1;
 		map[8][3] = 3;
 	};
-    this.lose = () => {
+	this.lose = () => {
+		playerMoved = false;
+		countdownStarted = false;
+		gameStarted = false;
+		gameStartedForProtagonist = false;
 		resetEnemies();
 		resetCountdown();
 		Swal.fire({
-			title: 'You lost!',
-			text: 'Someone killed you!',
-			icon: 'error',
-			confirmButtonText: 'Try again!'  
+			title: "You lost!",
+			text: "Someone killed you!",
+			icon: "error",
+			confirmButtonText: "Try again!",
 		}).then(() => {
 			restartLevel();
 		});
@@ -242,7 +276,7 @@ let player = function () {
 		this.key = false;
 		map[8][3] = 3;
 	};
-	
+
 	// This code controls the player's movement to find the key and open the door.
 	this.checkKeyAndDoor = () => {
 		let field = map[this.y][this.x];
@@ -261,41 +295,55 @@ let player = function () {
 			}
 		}
 	};
-    this.checkCollisionWithEnemy = (x, y) => {
-        if(this.x == x && this.y == y){
-            this.lose();
-        }
-    }
+	this.checkCollisionWithEnemy = (x, y) => {
+		if (this.x == x && this.y == y) {
+			this.lose();
+		}
+	};
 };
 
 const updateCountdown = () => {
-	if (protagonist.key) {
+	if (!countdownStarted) {
+		countdown = 31;
+	}
+
+	document.getElementById("countdownAudio").play(); // Start the music
+
+	if (!gameStartedForProtagonist) {
 		clearInterval(countdownInterval);
+		document.getElementById("countdownAudio").pause(); // Stop the music
 		return;
 	}
+
 	countdown--;
 	if (countdown === 0) {
 		clearInterval(countdownInterval);
 		protagonist.lose();
+		document.getElementById("countdownAudio").pause(); // Stop the music
 	}
-	document.getElementById("countdown").textContent = countdown + "s";
-};
 
+	document.getElementById("countdown").textContent = countdown + "s";
+
+	if (countdown === 30) {
+		document.getElementById("countdownAudio").play(); // Start the music
+	}
+};
 
 const resetCountdown = () => {
 	clearInterval(countdownInterval);
-	countdown = 30;
+	countdown = 31;
 	document.getElementById("countdown").textContent = countdown + "s";
 };
 
 const resetEnemies = () => {
 	enemies = [];
-	enemies.push(new enemy(3, 3));
-	enemies.push(new enemy(5, 7));
-	enemies.push(new enemy(7, 7));
+	enemies.push(new enemy(4, 4));
+	enemies.push(new enemy(13, 1));
+	enemies.push(new enemy(6, 8));
 };
 
 const restartLevel = () => {
+	gameStartedForProtagonist = true;
 	resetCountdown();
 	clearInterval(countdownInterval); // Detener el intervalo anterior
 	countdownInterval = setInterval(updateCountdown, 1000);
@@ -353,7 +401,6 @@ const init = () => {
 	}, 1000 / FPS);
 };
 
-
 // This code erases the canvas.
 const eraseCanvas = () => {
 	canvas.width = 750;
@@ -367,15 +414,17 @@ const main = () => {
 	protagonist.draw();
 
 	for (let i = 0; i < enemies.length; i++) {
-		enemies[i].moveRandomly();
 		enemies[i].draw();
+		if(gameStarted){
+			enemies[i].moveRandomly();
+		}
 	}
 
 	for (let i = 0; i < lights.length; i++) {
 		lights[i].draw();
 	}
 
-	if (protagonist.key) {
+	if (!gameStartedForProtagonist) {
 		clearInterval(countdownInterval);
 	}
 };
